1c9ec8b43b74a06bbcd5bc97703153941f32d7aa
Alex Walker <alexwalker2003master@gmail.com>
2025-08-31 23:56:47 +0100

    Added YFinance and Charts

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import yfinance as yf
+from flask import jsonify
 from flask import Flask, render_template
 import datetime
 
@@ -13,6 +15,16 @@ def home():
     }
     
     return render_template("index.html", data=data)
+
+@app.route("/api/get_volatility")
+def get_volatility():
+    ticker = "^VIX"
+    vix_data = yf.Ticker(ticker)
+    
+    #fetching the last month's daily volatility data to showcase trends on the about section
+    vix_info = vix_data.history(period="1m", interval="1d")
+    data = vix_info['Close'].reset_index().to_dict(orient='records')
+    return jsonify({"volatility_data": data})
 
 if __name__ == "__main__":
     app.run(debug=True)
